dbb31e55dbb3b4aa4353a86e1717199d5194e6f8
dpansubansal <dipansubansal@gmail.com>
2024-07-10 16:34:41 +0530

    merged wsd code

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/*!
+Amazon Kinesis Video Streams WebRTC SDK for JavaScript
+Copyright 2019-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+This product includes software developed at
+Amazon Web Services, Inc. (http://aws.amazon.com/).
+*/
+export type { Role } from './Role';
+export type { SignalingClient } from './SignalingClient';
+export type { SigV4RequestSigner } from './SigV4RequestSigner';
+export type { QueryParams } from './QueryParams';
+export type { RequestSigner } from './RequestSigner';
+
+export const VERSION = process.env.PACKAGE_VERSION;
